e3dbf6d25718aff4166628af30d64eb991914f15
Droid-MAX <Droid-MAX@users.noreply.github.com>
2023-11-25 00:16:26 +0800

    Update ssdp-query.py

--- ssdp-query.py
+++ ssdp-query.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
-import socket  
+import socket
 import sys
 
-dst = "239.255.255.250"  
-if len(sys.argv) > 1:  
+dst = "239.255.255.250"
+st = "upnp:rootdevice"
+
+if len(sys.argv) > 1:
     dst = sys.argv[1]
-st = "upnp:rootdevice"  
-if len(sys.argv) > 2:  
+if len(sys.argv) > 2:
     st = sys.argv[2]
 
-msg = [  
+msg = [
     'M-SEARCH * HTTP/1.1',
     'Host: 239.255.255.250:1900',
     f'ST: {st}',
@@ -17,11 +18,11 @@ msg = [
     'MX: 1',
     '']
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  
-s.settimeout(10)  
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+s.settimeout(10)
 s.sendto(('\r\n'.join(msg)).encode(), (dst, 1900))
 
-while True:  
+while True:
     try:
         data, addr = s.recvfrom(32*1024)
         print(f"[+] {addr}\n{data.decode()}")
