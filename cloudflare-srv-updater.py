b0fa97b23e926d10f43792d62b5981abe5e0de7e
Droid-MAX <Droid-MAX@users.noreply.github.com>
2023-12-29 15:55:59 +0800

    update some script

--- cloudflare-srv-updater.py
+++ cloudflare-srv-updater.py
@@ -1,23 +1,60 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+import argparse
 import requests
 import json
 import sys
 import os
 
-# Natter
-protocol=sys.argv[1]
-inner_ip=sys.argv[2]
-inner_port=sys.argv[3]
-outter_port=sys.argv[4]
+class Parser(argparse.ArgumentParser):
 
-# Cloudflare
-cf_api_token='CLOUDFLARE_API_TOKEN'
-cf_zone_id='CLOUDFLARE_ZONE_ID'
-cf_srv_name='_CLOUDFLARE_SERVICE_NAME'
-cf_domain_name='CLOUDFLARE_DOMAIN_NAME'
-cf_record_name=f'{cf_srv_name}._{protocol}.{cf_domain_name}'
-outter_host='EXAMPLE.COM'
+    def __init__(self):
+        super(Parser, self).__init__()
+
+    @staticmethod
+    def optparse():
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "-a", "--cf_api_token", dest="apiToken", metavar="cf_api_token", required=True,
+            help="specify the cloudflare api token (required)"
+        )
+        parser.add_argument(
+            "-i", "--cf_zone_id", dest="zoneId", metavar="cf_zone_id", required=True,
+            help="specify the cloudflare zone id (required)"
+        )
+        parser.add_argument(
+            "-s", "--cf_srv_name", dest="srvName", metavar="cf_srv_name", required=True,
+            help="specify the cloudflare service name, example: '_CLOUDFLARE_SERVICE_NAME' (required)"
+        )
+        parser.add_argument(
+            "-n", "--cf_domain_name", dest="domainName", metavar="cf_domain_name", required=True,
+            help="specify the cloudflare domain name (required)"
+        )
+        parser.add_argument(
+            "-m", "--protocol", dest="proto", metavar="protocol", default="tcp",
+            help="specify the protocol, tcp or udp (default: tcp)"
+        )
+        parser.add_argument(
+            "-t", "--host", dest="host", metavar="host", required=True,
+            help="specify the host name (required)"
+        )
+        parser.add_argument(
+            "-p", "--port", dest="port", metavar="port", required=True,
+            help="specify the port number (required)"
+        )
+        return parser.parse_args()
+
+opt = Parser().optparse()
+
+cf_api_token = opt.apiToken
+cf_zone_id = opt.zoneId
+cf_srv_name = opt.srvName
+cf_domain_name = opt.domainName
+protocol = opt.proto
+host = opt.host
+port = opt.port
+
+cf_record_name = f'{cf_srv_name}._{protocol}.{cf_domain_name}'
 
 def get_record_id(api_token, zone_id, record_name):
     url = (
@@ -72,7 +109,7 @@ def update_cloudflare_dns(
     try:
         response = requests.put(url, headers=headers, json=data)
         if response.status_code == 200:
-            print(f"[Script] - Upload to server: {protocol}: {inner_ip}:{inner_port} -> {outter_host}:{outter_port}")
+            print(f"Updated SRV record to Cloudflare: {cf_record_name} -> {host}:{port}")
         else:
             print(f"Error updating Cloudflare DNS: HTTP {response.status_code}")
         return response.json()
@@ -84,9 +121,9 @@ def update_cloudflare_dns(
         return None
 
 if __name__ == "__main__":
-    if not all([cf_api_token, cf_zone_id, cf_srv_name, cf_record_name]):
-        print("Missing one or more required variables.")
-        exit(1)
+    # if not all([cf_api_token, cf_zone_id, cf_srv_name, cf_record_name, host]):
+        # print("Missing one or more required variables.")
+        # exit(1)
     try:
         cf_record_id = get_record_id(cf_api_token, cf_zone_id, cf_record_name)
         update_response = update_cloudflare_dns(
@@ -94,7 +131,7 @@ if __name__ == "__main__":
             cf_zone_id,
             cf_record_id,
             cf_domain_name,
-            f"{outter_host}:{outter_port}",
+            f"{host}:{port}",
             cf_srv_name,
             f"_{protocol}",
         )
